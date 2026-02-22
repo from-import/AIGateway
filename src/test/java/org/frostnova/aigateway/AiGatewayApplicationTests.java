ee1ff8612a473e56c5947542c408060cb464db9d
from-import <dsearses6574@gmail.com>
2026-02-22 18:06:34 +0800

    feat: create the AIGateway project

---
+++ src/test/java/org/frostnova/aigateway/AiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.frostnova.aigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
