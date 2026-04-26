eb50a73ff2527dfd15c7998b3656cbf4f608545b
from-import <dsearses6574@gmail.com>
2026-04-26 23:06:31 +0800

    feat:实现分布式异步任务回调框架

--- src/main/java/org/frostnova/aigateway/AiGatewayApplication.java
+++ src/main/java/org/frostnova/aigateway/AiGatewayApplication.java
@@ -3,9 +3,10 @@ package org.frostnova.aigateway;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
 @Slf4j
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AiGatewayApplication {
 
     public static void main(String[] args) {
